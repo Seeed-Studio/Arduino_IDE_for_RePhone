f8acfde4ecf8df8e40b961bf6be28e2d8d22d284
WayneWeng <jinyuan.weng@seeed.cc>
2016-07-19 13:47:32 +0800

    Update
    Fix the bug of NFC example.

--- hardware/arduino/mtk/libraries/LNFC/examples/nfc_test/nfc_test.ino
+++ hardware/arduino/mtk/libraries/LNFC/examples/nfc_test/nfc_test.ino
@@ -7,6 +7,20 @@ void setup()
 {
     Serial.begin(115200);
     Serial.print("NFC test.\r\n");
+    
+    while(1)
+    {
+        if(LNFC.check_on_line())
+        {
+            Serial.println("On-line!");
+            break;
+        }
+        else Serial.println("Off-line!");
+        delay(1000);
+    }
+    
+    LNFC.adapter_init();
+    delay(1000);
 }
     
 void loop()
@@ -14,13 +28,10 @@ void loop()
     unsigned char *str = 0;
     char buffer[50] = {0,};
     
-    if(LNFC.check_online())
-    {
-        LNFC.adapter_init();
-        str = LNFC.adapter_get_uid();
-        sprintf(buffer, "NFC uid is 0x%x 0x%x 0x%x 0x%x\r\n", str[2],str[3],str[4],str[5]);
-        Serial.print(buffer);
-    }
+    str = LNFC.adapter_get_uid();
+    sprintf(buffer, "NFC uid is 0x%x 0x%x 0x%x 0x%x\r\n", str[2],str[3],str[4],str[5]);
+    Serial.print(buffer);
+    
     delay(1000);
 }
 
